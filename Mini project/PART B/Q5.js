23999ea5c48a9546094d891a09bb6f85b2ed4edc
harshm08-coder <h15august75@gmail.com>
2026-03-19 10:24:30 +0530

    Delete Mini project directory

--- Mini project/PART B/Q5.js
+++
@@ -1,6 +0,0 @@
-function isPalindrome(){
-    let str = "madam";
-    let reversed = str.split("").reverse().join("");  
-    return str === reversed;
-}
-console.log("The Array is palindrome:"+isPalindrome());
